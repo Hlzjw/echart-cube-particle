a5b93a685e5334adae2001cbe0346881f70849c8
hl <17609394441@189.cn>
2023-10-26 11:13:24 +0800

    fix:删除无用文件

--- src/export/components.js
+++ src/export/components.js
@@ -41,23 +41,5 @@
 * specific language governing permissions and limitations
 * under the License.
 */
-export { install as GridSimpleComponent } from '../component/grid/installSimple.js';
-export { install as GridComponent } from '../component/grid/install.js';
-export { install as PolarComponent } from '../component/polar/install.js';
-export { install as GeoComponent } from '../component/geo/install.js';
-export { install as SingleAxisComponent } from '../component/singleAxis/install.js';
-
-export { install as TooltipComponent } from '../component/tooltip/install.js';
-export { install as AxisPointerComponent } from '../component/axisPointer/install.js';
-
-export { install as MarkLineComponent } from '../component/marker/installMarkLine.js';
-export { install as LegendComponent } from '../component/legend/install.js';
-export { install as LegendScrollComponent } from '../component/legend/installLegendScroll.js';
-export { install as LegendPlainComponent } from '../component/legend/installLegendPlain.js';
-export { install as DataZoomComponent } from '../component/dataZoom/install.js';
-export { install as DataZoomSliderComponent } from '../component/dataZoom/installDataZoomSlider.js';
-export { install as VisualMapComponent } from '../component/visualMap/install.js';
-export { install as VisualMapContinuousComponent } from '../component/visualMap/installVisualMapContinuous.js';
-export { install as VisualMapPiecewiseComponent } from '../component/visualMap/installVisualMapPiecewise.js';
 export { install as ParticleComponent } from '../component/particle/install.js';
 export { install as LightComponent } from '../component/light/install.js';
